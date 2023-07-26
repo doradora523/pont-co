ee2ade4eab0e9fd0dec69d528e64b35983e56adf
doradora523 <doradora523@naver.com>
2023-07-26 17:21:21 +0900

    chore: SCSS/gulp 설치 및 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      pont-co    
-    </div>
-  );
+  return <div className="App">pont-co</div>;
 }
 
 export default App;
